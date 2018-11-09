ea3de4e74e81dc569ee228c377b2110dd76f916a
Kazbek <kazbek.se@gmail.com>
2018-11-09 19:49:34 +0300

    Добавьте файлы проекта.

---
+++ TemplateBasedDocumentGenerator/DocxGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xceed.Words.NET;
+
+namespace TemplateBasedDocumentGenerator
+{
+    public class DocxGenerator
+    {
+        public static void FillTemplate(in Stream docxInputFileStream, Stream docxResultWriteStream, Dictionary<string, string> toReplace)
+        {
+            DocX doc = DocX.Load(docxInputFileStream);
+            foreach (KeyValuePair<string, string> replace in toReplace)
+            {
+                doc.ReplaceText(replace.Key, replace.Value);
+            }
+            
+            doc.SaveAs(docxResultWriteStream);
+        }
+    }
+}
