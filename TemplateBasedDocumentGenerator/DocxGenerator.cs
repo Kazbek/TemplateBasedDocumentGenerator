66b00df5bbf6af6337964095c752e38b3513b3e6
Kazbek <kazbek.se@gmail.com>
2018-11-09 20:02:54 +0300

    Перегрузки для работы с файлами и потоками.

--- TemplateBasedDocumentGenerator/DocxGenerator.cs
+++ TemplateBasedDocumentGenerator/DocxGenerator.cs
@@ -1,22 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using Xceed.Words.NET;
 
 namespace TemplateBasedDocumentGenerator
 {
     public class DocxGenerator
     {
-        public static void FillTemplate(in Stream docxInputFileStream, Stream docxResultWriteStream, Dictionary<string, string> toReplace)
+        public static void FillTemplate(in Stream docxTemplateFileStream, Stream docxResultWriteStream, in Dictionary<string, string> toReplace)
         {
-            DocX doc = DocX.Load(docxInputFileStream);
-            foreach (KeyValuePair<string, string> replace in toReplace)
-            {
-                doc.ReplaceText(replace.Key, replace.Value);
-            }
+            DocX doc = DocX.Load(docxTemplateFileStream);
+            FillTemplate(doc, toReplace);
             
             doc.SaveAs(docxResultWriteStream);
+        }
+
+        public static void FillTemplate(in string docxTemplateFilePath, string docxResultFilePath, in Dictionary<string, string> toReplace)
+        {
+            DocX doc = DocX.Load(docxTemplateFilePath);
+            FillTemplate(doc, toReplace);
+
+            doc.SaveAs(docxResultFilePath);
+        }
+
+        public static void FillTemplate(in string docxTemplateFilePath, Stream docxResultWriteStream, in Dictionary<string, string> toReplace)
+        {
+            DocX doc = DocX.Load(docxTemplateFilePath);
+            FillTemplate(doc, toReplace);
+
+            doc.SaveAs(docxResultWriteStream);
+        }
+
+        public static void FillTemplate(in Stream docxTemplateFileStream, string docxResultFilePath, in Dictionary<string, string> toReplace)
+        {
+            DocX doc = DocX.Load(docxTemplateFileStream);
+            FillTemplate(doc, toReplace);
+
+            doc.SaveAs(docxResultFilePath);
+        }
+
+        private static void FillTemplate(DocX template, in Dictionary<string, string> toReplace)
+        {
+            foreach (KeyValuePair<string, string> replace in toReplace)
+            {
+                template.ReplaceText(replace.Key, replace.Value);
+            }
         }
     }
 }
